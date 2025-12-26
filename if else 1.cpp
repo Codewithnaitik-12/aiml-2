8e3819cf7d5abd5e3f0efb4d2a0f1e9edb09e508
Codewithnaitik-12 <naittik3106@gmail.com>
2025-12-26 13:28:34 +0530

    Add files via upload

---
+++ if else 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+cin>>a;
+cout<<"enter the value of a"<<endl;
+if(a>0){
+	cout<<"a is positive"<<endl;
+}else if(a<0){
+cout<<"a is negative"<<endl;
+} 
+else {cout<<"a is zero"<<endl;
+}
+return 0;
+}
