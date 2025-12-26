491e2d5df24172ab9466b3c3f18910b4d4bea8b4
Codewithnaitik-12 <naittik3106@gmail.com>
2025-12-26 13:27:34 +0530

    Add files via upload

---
+++ function swap 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void swap(int a,int b){
+
+int temp;
+temp=a;
+a=b;
+b=temp;}
+int main(){
+	int a=4;
+	int b=5;
+	cout<<a<<" "<<b<<endl;
+	swap(a,b);
+	cout<<a<<" "<<b<<endl;
+	
+}
