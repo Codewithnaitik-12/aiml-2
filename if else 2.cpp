820852b419b45cc0f89232a0f9b1894a7e46e687
Codewithnaitik-12 <naittik3106@gmail.com>
2025-12-26 13:28:57 +0530

    Add files via upload

---
+++ if else 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int age;
+	cin>>age;
+	cout<<"enter the value of age"<<endl;
+	if(age>40){
+		cout<<"person is old"<<endl;
+	}
+	else if(age<40){
+		cout<<"person is young"<<endl;
+	}
+	else{
+	 cout<<"person is young and older";
+}
+}
